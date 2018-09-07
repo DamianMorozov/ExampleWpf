fd2300cffe97be984b691ca177d8acd697f71b85
DamianMorozov <web@morozov33.ru>
2018-09-07 18:11:01 +0300

    Add basic windows.
    - with Grid
    - with UniformGrid
    - with GridSplitter
    - with StackPanel

---
+++ WpfApp/WindowMain.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(FieldSome.Text);
+        }
+
+        private void ButtonCreateNewWindow_Click(object sender, RoutedEventArgs e)
+        {
+            var windowUniformGrid = new WindowUniformGrid
+            {
+                Left = Left + Width,
+                Top = Top,
+            };
+            windowUniformGrid.ShowDialog();
+        }
+
+        private void ButtonCreateGridSplitter_Click(object sender, RoutedEventArgs e)
+        {
+            var windowGridSplitter = new WindowGridSplitter
+            {
+                Left = Left + Width,
+                Top = Top,
+            };
+            windowGridSplitter.ShowDialog();
+        }
+
+        private void ButtonCreateStackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            var windowStackPanel = new WindowStackPanel
+            {
+                Left = Left + Width,
+                Top = Top,
+            };
+            windowStackPanel.ShowDialog();
+        }
+    }
+}
---
+++ WpfApp/WindowUniformGrid.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for WindowUniformGrid.xaml
+    /// </summary>
+    public partial class WindowUniformGrid : Window
+    {
+        public WindowUniformGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
